a0f0c15fa2ecf89c057b0149a207467b4780cd95
Ryan <ryantheterror@gmail.com>
2023-09-28 19:08:06 -0400

    updated duedates to handle all the courses

--- commands/duedates.py
+++ commands/duedates.py
@@ -2,9 +2,14 @@ import json
 from datetime import datetime, timedelta
 from commands.base import CommandHandler
 
+file_paths = [
+    "data/cloudAdminSchedule.json",
+    "data/devops_schedule.json",
+    "data/python_scedule.json"
+]
+
 class CohortDueDates(CommandHandler):
-    async def assignment_due_dates(self, message, match_list=None):
-        file_path = 'data/cloudAdminSchedule.json'
+    async def assignment_due_dates(self, message, file_path, match_list=None):
         with open(file_path, 'r') as f:
             data = json.load(f)
 
@@ -16,10 +21,20 @@ class CohortDueDates(CommandHandler):
             if 'modules' in unit:
                 for module in unit['modules']:
                     if module['moduleType'] in ['PROJECT', 'FORUM', 'QUIZ']:
+                        # First, try the 'T' separated date-time format
                         try:
                             item_end_date = datetime.strptime(module['endDate'], '%Y-%m-%dT%H:%M:%S').date()
                         except ValueError:
-                            item_end_date = datetime.strptime(module['endDate'], '%Y-%m-%dT%H:%M').date()
+                            try:
+                                item_end_date = datetime.strptime(module['endDate'], '%Y-%m-%dT%H:%M').date()
+                            except ValueError:
+                                # If the above fails, try the AM/PM date-time format
+                                try:
+                                    item_end_date = datetime.strptime(module['endDate'], '%m/%d/%Y %I:%M %p').date()
+                                except ValueError:
+                                    # If all the above fail, skip items with empty or unrecognized date formats
+                                    continue
+                        
                         if current_date <= item_end_date <= week_end_date:
                             item_info = f"Item Type: {module['moduleType']}\n"
                             item_info += f"Item Name: {module['name']}\n"
@@ -29,8 +44,18 @@ class CohortDueDates(CommandHandler):
                             items_due_this_week.append((item_end_date, item_info))
 
         items_due_this_week.sort(key=lambda x: x[0])
-
         sorted_items_info = [item[1] for item in items_due_this_week]
-
         response = "\n".join(sorted_items_info)
-        await self.bot.send_message(message['rid'], response)
+        
+        return response
+
+    async def handle_multiple_files(self, message, match_list=None):
+        all_due_dates = []
+        
+        for file_path in file_paths:
+            due_dates_for_file = await self.assignment_due_dates(message, file_path, match_list)
+            if due_dates_for_file:  # Only append if there are due dates for this file
+                all_due_dates.append(due_dates_for_file)
+        
+        combined_due_dates = "\n\n".join(all_due_dates)
+        await self.bot.send_message(message['rid'], combined_due_dates)
--- commands/handler.py
+++ commands/handler.py
@@ -14,4 +14,4 @@ class MainCommandHandler:
         self.bot.register_command(r'!askllama (.*)', self.huggingchat_commands.ask_huggingface, description="Ask the llama2 model a question. Model hosted by huggingface.")
         self.bot.register_command(r'!ping', self.general_commands.ping_command, description="Checks if bot is running")
         self.bot.register_command(r'!help', self.general_commands.list_commands, description="Lists all available commands")
-        self.bot.register_command(r'!deadlines', self.cohort_due_dates_commands.assignment_due_dates, description="Lists graded items due within the week")
+        self.bot.register_command(r'!deadlines', self.cohort_due_dates_commands.handle_multiple_files, description="Lists graded items due within the week")
